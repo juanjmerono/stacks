f047893924a1dbae2eda43dc4bf486ae66aced08
juanjmerono <jjmerono@um.es>
2017-10-28 23:13:16 +0200

    Uncomment one line.

--- download_images.sh
+++ download_images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker run --rm -it -v ${PWD}:/app -w /app node:8.2.1-alpine npm install
+docker run --rm -it -v ${PWD}:/app -w /app node:8.2.1-alpine npm install
 docker run --rm -it -v ${PWD}:/app -v /var/run/docker.sock:/var/run/docker.sock -w /app node:8.2.1-alpine node download.js $*
